48ac866fdcfc8d47357387ada2072c2be1430f56
uta0414 <s.uta0414@gmail.com>
2024-01-22 16:24:50 +0900

    google認証でログインを実装(コンフリクト解消)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 import Login from "./api/auth/login";
 
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      password: "abcdefg1234",
+    },
+  });
+  console.log(user);
+}
+
 export default function Home() {
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+
   return (
     <>
       {/*
